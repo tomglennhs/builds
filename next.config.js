d641dd480fc844e9c1139f612954fc48e176f9c9
Jason Antwi-Appah <hey@jasonaa.me>
2021-03-13 17:13:03 +0000

    chore: add env vars

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	target: "serverless",
 	async rewrites() {
 		return [
 			// TODO: Eventually I'll want to make an API method that use fetch() to get the file and returns it, since the download attribute only lets me specify a custom filename on same origin urls.
@@ -8,4 +9,14 @@ module.exports = {
 			},
 		]
 	},
+	env: {
+		teamRepo: 'grizzlybots11918/ultimategoal',
+		teamDefaultBranch: "master",
+		teamBuildWorkflow: '.github/workflows/format.yml',
+		upstreamRepo: 'FIRST-Tech-Challenge/FtcRobotController',
+		NEXT_PUBLIC_ACCENT: '#ef4b24',
+		// color scheme can be any of "orange" | "whiteAlpha" | "blackAlpha" | "gray" | "red" | "yellow" | "green" | "teal" | "blue" | "cyan" | "purple" | "pink" | "linkedin" | "facebook" | "messenger" | "whatsapp" | "twitter" | "telegram"
+		NEXT_PUBLIC_COLOR_SCHEME: "orange",
+
+	  },
 }
