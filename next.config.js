7e84b79ccf2ed38762a4aa4e39e9dd3629c20971
Jason Antwi-Appah <hey@jasonaa.me>
2021-04-27 19:15:06 -0500

    fix: send correct content type for apks
    Fixes issue where downloaded APKs aren't installable.

--- next.config.js
+++ next.config.js
@@ -8,5 +8,18 @@ module.exports = {
         NEXT_PUBLIC_ACCENT: '#ef4b24',
         // color scheme must be one of "orange" | "whiteAlpha" | "blackAlpha" | "gray" | "red" | "yellow" | "green" | "teal" | "blue" | "cyan" | "purple" | "pink" | "linkedin" | "facebook" | "messenger" | "whatsapp" | "twitter" | "telegram"
         NEXT_PUBLIC_COLOR_SCHEME: 'orange'
+    },
+    async headers() {
+        return [
+          {
+            source: '/api/downloadDs',
+            headers: [
+              {
+                key: 'Content-Type',
+                value: 'application/vnd.android.package-archive'
+              }
+            ]
+          }
+        ]
     }
 }
